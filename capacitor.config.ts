908a5c521a8eab3e39c05e0a72dd5c899ec0eae2
Devanshisbroke <arraybhaiarray@gmail.com>
2026-04-06 20:05:48 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.terracore.globeid',
+  appName: 'GlobeID',
+  webDir: 'dist'
+};
+
+export default config;
